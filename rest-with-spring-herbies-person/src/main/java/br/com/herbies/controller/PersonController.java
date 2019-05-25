edaa0c6dddce441729fb9e12bde8a2ab4684c7d7
DESKTOP-MA2BPC6\Herbert <herbertfilhik@gmail.com>
2019-05-25 11:04:28 -0300

    ajustando o controller para receber Long ao inves de String

--- rest-with-spring-herbies-person/src/main/java/br/com/herbies/controller/PersonController.java
+++ rest-with-spring-herbies-person/src/main/java/br/com/herbies/controller/PersonController.java
@@ -29,7 +29,7 @@ public class PersonController {
 	@RequestMapping(value="/{id}", 
 			method=RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public PersonEntity findById(@PathVariable("id") String id) {
+	public PersonEntity findById(@PathVariable("id") Long id) {
 		return services.findById(id);
 	}
 	
@@ -49,7 +49,7 @@ public class PersonController {
 	 
 	@RequestMapping(value="/{id}", 
 			method=RequestMethod.DELETE)
-	public void delete(@PathVariable("id") String id) {
+	public void delete(@PathVariable("id") Long id) {
 		services.delete(id);
 	}
 }
